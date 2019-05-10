b90c937be95ddc5053210d1d12ef4e852b8baa39
Wachirawit Iedchoo <wachirawit.iedchoo@gmail.com>
2019-05-10 21:52:53 +0700

    add Thai letters

--- CRNN/config.py
+++ CRNN/config.py
@@ -1,7 +1,12 @@
 # Constants
 
 # Supported characters
-CHAR_VECTOR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-'.!?,\""
+CHAR_VECTOR = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-'.!?,\"")
+
+thai_alphabets = [chr(i) for i in range(ord('ก'), ord('ฮ'))]
+thai_vowels = [chr(i) for i in range(ord('ะ'), ord('์'))]
+
+CHAR_VECTOR = CHAR_VECTOR + thai_alphabets + thai_vowels
 
 # Number of classes
 NUM_CLASSES = len(CHAR_VECTOR) + 1
